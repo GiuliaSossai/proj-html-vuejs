3aad8d65cb2ab52021dc6a40af99354a0d5048c6
GiuliaSossai <giulia.sossai@yahoo.it>
2021-12-13 17:04:08 +0100

    prima e seconda sezione

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -21,10 +21,12 @@
             </ul>
          </div>
       </div>
+
+      
       
       <div class="jumbotron">
          <div class="container">
-            <div class="d-flex justify-content-between align-items-center pt-3">
+            <div class="d-flex justify-content-between align-items-center pt-3 jumbo-top">
                <div class="logo">
                   <a href="#"><img src="../assets/img/nexgen-logo.svg" alt="nexgen-logo"></a>
                </div>
@@ -44,11 +46,15 @@
                </ul>   
             </div>
 
-            <h1 class="text-center">Financial Risk</h1>
-            <p class="text-center">The right outcomes depends on continuos rigor in governance, models, and processes across the finance function.</p>
-
-            <button>get in touch</button>
-            <button>read more</button>
+            <div class="jumbo-bottom">
+               <h1 class="text-center">Financial Risk</h1>
+               <p class="text-center">The right outcomes depends on continuos rigor in governance, models, and processes across the finance function.</p>
+               <div class="text-center pt-4">
+                  <button class="btn gs-solid-btn">get in touch</button>
+                  <button class="btn gs-btn">read more</button>
+               </div>   
+            </div>
+            
          </div>
          
       </div>
@@ -98,28 +104,50 @@ header {
       height: 600px;
       background-image: url('../assets/img/bg-2.jpg');
       @include bg-position;
-      .logo {
+      .jumbo-top {
+         margin-bottom: 10%;
+         .logo {
          width: 120px;
-      }
+         }
 
-      .menu {
-         text-transform: uppercase;
-         a {
-            margin: 0 20px;
-            color: lighten($subtitle, 20%);
-            &:hover {
-            color: $subtitle;
+         .menu {
+            text-transform: uppercase;
+            a {
+               padding: 0 20px;
+               color: lighten($subtitle, 20%);
+               &:hover {
+               color: $subtitle;
+               }
+            }
+            .active {
+               background-color: $main-btn;
             }
          }
-         .active {
-            background-color: $main-btn;
-         }
       }
 
-      h1 {
-         margin-top: 20%;
-         color: $main;
-      }
+      .jumbo-bottom{
+         @include gs-wrap;
+         h1 {
+            font-size: 60px;
+            margin-bottom: 20px;
+            color: $main;
+         }
+         p{
+            font-size: 20px;
+         }
+         .gs-solid-btn{
+            color: $main;
+            background-color: $main-btn;
+            margin-right: 10px;
+         }
+         .gs-btn {
+            color: $main;
+            border: 1px solid $main-btn;
+            &:hover {
+               background-color: $main-btn;
+            }
+         }
+      }  
    }
 }
 
